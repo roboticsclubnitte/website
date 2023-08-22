9d1d1c0cb3d222c16402323ce35d57de4d57f028
Taksheelshetty <120228028+Taksheelshetty@users.noreply.github.com>
2023-08-22 20:57:03 +0530

    Fix google signin

--- main.js
+++ main.js
@@ -10,21 +10,28 @@ axios
       var head = document.getElementById("head");
       var button = document.getElementById("button_1");
       var buttonlink = button.getElementsByTagName("a");
-      buttonlink.href = "https://dashboard.roboticsclubnitte.com";
       var content = document.getElementById("content");
       const profileComponent = document.createElement("li");
       const Link = document.createElement("a");
       Link.className = "li-btn";
       Link.id = "nav-btn";
       profileComponent.appendChild(Link);
-      profileComponent.addEventListener('click',login)
+      profileComponent.addEventListener("click", login);
       Link.innerHTML = "Profile";
       const navList = document.getElementById("nav-list");
       navList.appendChild(profileComponent);
       content.textContent =
         " Club designs, builds, and creates bots. Conducts workshops on Bot building, IoT, 3D modeling, and Programming. Works on diverse projects.";
-      head.textContent = "View Your Dashbord By Clicking below";
-      button.textContent = "Dasboard";
+      if (res.data.paymentStatus === "RECEIVED") {
+        buttonlink.href = "https://dashboard.roboticsclubnitte.com";
+        head.textContent = "View Your Dashbord By Clicking below";
+        button.textContent = "Dasboard";
+      } else {
+        buttonlink.href = "#about";
+        head.textContent = "Get Kown More About Us";
+        button.textContent = "GET KOWN US";
+        alert("Payment not verified!");
+      }
     }
   })
   .catch((error) => {
@@ -34,19 +41,20 @@ axios
     Link.id = "nav-btn";
     profileComponent.appendChild(Link);
     Link.innerHTML = "Login/Register";
-    profileComponent.addEventListener('click',login)
+    profileComponent.addEventListener("click", login);
     const navList = document.getElementById("nav-list");
     navList.appendChild(profileComponent);
     console.log(error);
   });
 function login() {
   axios
-    .post("https://rbclub-server.onrender.com/api/auth/register", {},
-      {withCredentials: true},
+    .post(
+      "https://rbclub-server.onrender.com/api/auth/register",
+      {},
+      { withCredentials: true }
     )
     .then((result) => {
       if (result.data.message === "success") {
-        console.log(result.data.authUrl)
         window.location.replace(result.data.authUrl);
       }
     })
